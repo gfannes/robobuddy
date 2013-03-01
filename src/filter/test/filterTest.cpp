d171ca72e7e7c6630735abc7ac4013c5782df309
RoboBuddie <alain.vermeren@telenet.be>
2013-03-01 21:22:11 +0100

    Added datasheets

--- src/filter/test/filterTest.cpp
+++ src/filter/test/filterTest.cpp
@@ -1,6 +1,8 @@
 #include "filter/MinMax.h"
 #include "filter/Invert.h"
 #include "filter/Offset.h"
+#include "filter/Samples.h"
+
 #include <iostream>
 using namespace filter;
 using namespace std;
@@ -25,6 +27,19 @@ int main(int argc, const char* argv[])
 	cout << "--- Chained Filter ---" << endl;
 	for(int i = -30; i <= 30; i += 5)
 		cout << i << " -> " << mm(off(inv(i))) << endl;
+	
+	cout << "--- Averaging Filter ---" << endl;
+	Samples<int, 5> samples;
+	
+	samples.add(4);
+	samples.add(5);
+	samples.add(6);
+	cout << samples.average() << endl;
+	samples.add(7);
+	samples.add(8);
+	samples.add(9);
+	cout << samples.average() << endl;
+	cout << "--- End ---" << endl;
 	
 	return 0;
 }
